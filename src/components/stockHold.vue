d25f77947fc735d90134811a6be3d85ca80dd274
tangxuning <tangxuning@huoban.com>
2022-09-12 05:23:32 +0800

    完成子表数据的动态加载

--- src/components/stockHold.vue
+++ src/components/stockHold.vue
@@ -3,11 +3,18 @@
     <a-table 
         :data-source="data" 
         :columns="columns"        
-        :loading="loading" 
-        @expand="expandCustomRow"       
+        :loading="loading"
+        @expand="expandCustomRow" 
+        :expandedRowKeys="expandedRowKeys"
      >
         <template #expandedRowRender>
-        
+            <a-table 
+                :columns="innerColumns" 
+                :data-source="innerData" 
+                :pagination="false"
+                :loading="innerLoading"
+            >
+            </a-table>        
         </template>
     </a-table>
     
@@ -15,7 +22,7 @@
 <script>
 import * as echarts from 'echarts';
 //import { DownOutlined } from '@ant-design/icons-vue';
-import { reqGetHoldStocks } from '@/apis/stock';
+import { reqGetHoldStocks ,reqGetStockHistory} from '@/apis/stock';
 //import { usePagination } from 'vue-request';
 //import axios from 'axios';
 
@@ -37,6 +44,7 @@ const columns = [{
     },{
         title: '市值价',
         dataIndex: 'marketVule',
+        defaultSortOrder:'descend',
         sorter: (a,b)=>a.marketVule-b.marketVule,
     },{
         title: '市场',
@@ -47,22 +55,35 @@ const innerColumns =[
     {
         title: '日期',
         dataIndex: 'date',
+        sorter: (a,b)=>{
+            let aTime = new Date(a.date).getTime();
+            let bTime = new Date(b.date).getTime();
+            return aTime - bTime;
+        }
     },
     {
         title: '时间',
         dataIndex: 'time',
+        defaultSortOrder:'descend',
+        sorter: (a,b)=>{
+            let aTime = new Date(a.date+' '+a.time).getTime();
+            let bTime = new Date(b.date+' '+b.time).getTime();
+            return aTime - bTime;
+        }
     },
     {
         title: '买卖方向',
-        dataIndex: 'sellBye',
+        dataIndex: 'sell_buy',
     },
     {
         title: '交易价格',
         dataIndex: 'price',
+        sorter: (a,b)=>a.price-b.price,
     },
     {
         title: '成交量',
         dataIndex: 'num',
+        sorter: (a,b)=>a.num-b.num,
     },
 ];
 
@@ -70,24 +91,51 @@ export  default ({
     components:{
         //DownOutlined,
     },
-    setup(){ 
-        
-        const expandCustomRow = (expanded, record) => {
-            console.log(expanded);
-            console.log(record);
-        }
-        
+    setup(){
+               
         return {            
-            expandCustomRow,
+            
         };
     },
     methods:{
+        /**
+         * 获取表格数据
+         */
         async getTableData(){
             this.loading = true;
             const res = await reqGetHoldStocks();
             //console.log(res.data);
             this.data = res.data;
             this.loading = false;
+        },
+        /**
+         * 根据股票代码，市场参数获取股票交易历史数据
+         * @params {code} 6位股票代码
+         * @params {market} 市场
+         */
+        async getInnerTableData (code,market){
+            this.innerLoading = true;
+            const res = await reqGetStockHistory({code:code,market:market});
+            this.innerLoading = false;
+            this.innerData = res.data;
+        },
+        expandCustomRow(expanded, record) {
+            if (this.expandedRowKeys.length > 0) { //进这个判断说明当前已经有展开的了
+            //返回某个指定的字符串值在字符串中首次出现的位置，下标为0
+                let index = this.expandedRowKeys.indexOf(record.key);
+                if (index > -1) { //如果出现则截取这个id,1d到1相当于0，针对重复点击一个
+                    this.expandedRowKeys.splice(index, 1);
+                } else {
+                //如果没出现则截取所有id,添加点击id，0到1，针对已经有一个展开，点另一个会进入判断
+                this.expandedRowKeys.splice(0, this.expandedRowKeys.length);
+                this.expandedRowKeys.push(record.key);
+                }
+            } else {
+            //数组长度小于0，说明都没展开，第一次点击，id添加到数组，数组有谁的id谁就展开
+                this.expandedRowKeys.push(record.key);  
+            }
+            if(expanded === true)
+                this.getInnerTableData(record.code,record.market);
         },
         drawEtharts(){
             let myChart = echarts.init(document.getElementById("myEchart"));
@@ -128,9 +176,12 @@ export  default ({
     data(){
         return{
             data: [],
+            innerData:[],
             columns,
             innerColumns,
             loading:false,
+            innerLoading:false,
+            expandedRowKeys:[],
         }
     }
     
